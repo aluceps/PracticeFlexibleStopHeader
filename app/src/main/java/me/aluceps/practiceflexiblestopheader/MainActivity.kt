262c514bf3d624e36e5ab4db0fcb6f3acf436f24
aluceps <aluceps@gmail.com>
2018-10-17 15:53:42 +0900

    add toolbar

--- app/src/main/java/me/aluceps/practiceflexiblestopheader/MainActivity.kt
+++ app/src/main/java/me/aluceps/practiceflexiblestopheader/MainActivity.kt
@@ -1,12 +1,21 @@
 package me.aluceps.practiceflexiblestopheader
 
-import android.support.v7.app.AppCompatActivity
+import android.databinding.DataBindingUtil
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import me.aluceps.practiceflexiblestopheader.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private val binding by lazy {
+        DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setSupportActionBar(binding.toolbar)
+        supportFragmentManager.beginTransaction()
+                .replace(binding.container.id, MainFragment.newInstance())
+                .commitNow()
     }
 }
